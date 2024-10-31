36fb677ff6a97e0407fd096ba2be440f2bc99640
StaticRevo <isaacattard@hotmail.com>
2024-10-31 17:58:34 +0100

    31/10/2024

---
+++ FYPProject/DataPreprocessing.py
@@ -0,0 +1,12 @@
+def BigEarthNetDataPreprocessing():
+    # Stage 1: Download the dataset
+    # Stage 2: Extract the dataset
+    # Stage 3: Remove uncessary files and folders based on the provided metadata
+    # Stage 4: Add a binary vector to the metadata file to indicate the presence of a specific land cover class
+    # Stage 5: Resize the TIFF files to the same resolution (120x120)
+    # Stage 6: Combine TIFF files into a single image
+    # Stage 7: Split the dataset into training, validation and testing sets
+    # Stage 8: Apply normalisation and data augmentation techniques
+    # Stage 9: Save the preprocessed dataset to a specified location
+    
+    pass
